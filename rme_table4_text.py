7a702e8ed088092afbcf8a5101e072befcb2b854
tbxy09 <tb_xy09@163.com>
2019-11-21 15:52:05 +0800

    add

---
+++ rme_table4_text.py
@@ -0,0 +1,55 @@
+
+tr_li_history=[]
+tr_li_history.append({
+'work_time':
+        "2019.1.17-Now",
+'work_company':
+        " 真玫智能 ",
+'work_industry':
+        "边缘计算/VR/AR/机器人",
+'work_title':
+        " 机器学习算法工程师",
+'work_desp':
+        " 公司主要从事服装产业链的智能化，包含各种模型开发和数据库的建立。\
+ 负责的项目包换 3D虚拟试衣镜的human body tracking/motion capture 和 \
+可穿衣的虚拟模特的 animation模块，基于source seq 是 mobile高清摄像头采集的 monocular video stream \
+（不包含任何传感数据），同时还负责公司工业机器人部门的基于图像和强化的机械手开发。 "
+ }
+)
+
+tr_li_history.append({
+'work_time':
+        "2019.1.17-Now",
+'work_company':
+        " 真玫智能 ",
+'work_industry':
+        "边缘计算/VR/AR/机器人",
+'work_title':
+        " 机器学习算法工程师",
+'work_desp':
+        " 公司主要从事服装产业链的智能化，包含各种模型开发和数据库的建立。\
+ 负责的项目包换 3D虚拟试衣镜的human body tracking/motion capture 和 \
+可穿衣的虚拟模特的 animation模块，基于source seq 是 mobile高清摄像头采集的 monocular video stream \
+（不包含任何传感数据），同时还负责公司工业机器人部门的基于图像和强化的机械手开发。 "
+ }
+)
+
+tr_li_proj = []
+tr_li_proj.append({
+'proj_name':
+        "2019.4-Now 3D虚拟试衣镜的human body tracking 和 可穿衣的虚拟模特的 animation模块",
+'proj_desp':
+        " 项目是为了实现一套on-device的算法，部署在一个edge系统上，通过从高清相机中采集的video \
+                   stream,来对non-Neural body进行 tracking/animation其中主要的工作scope包含以下几点：build and \
+                   training body tracking network;build and training body animation/imitation network;build metrics \
+                   to measure the loss of both 2d and 3d features;training dataset preparation;AR Location/Fool \
+                   detection 方案研究;movement smooth and jitters remove", \
+'proj_per':
+        "而遇到的挑战主要包含 在model design 方面； model-free/model-base 方案选择,深度信息的模糊性depth ambiguity \
+                   of image，dense rotation supervision data generation，location/rotation jitter problem，chest \
+                   turn specific movement accuracy ，在 Inference 方面；less dependency to the network \
+                   connection，video tracking/animation/clothing all run together in edge device，Inference \
+                   latency/performance tradeoff",
+ }
+)
+
